7310dbf7b14486af5aa39f4c651576f734086fb7
WilfredSalmon <was29@cam.ac.uk>
2024-08-20 14:24:54 +0100

    added test for file already existing

--- Gui/app.py
+++ Gui/app.py
@@ -1,6 +1,7 @@
 from PyQt6 import QtWidgets, QtGui, QtCore
 from PyQt6.QtCore import Qt
 from shutil import copy2
+from pathlib import Path
 
 STYLE_SHEET = """
     QWidget
@@ -19,9 +20,22 @@ class Main_Window(QtWidgets.QMainWindow):
         self.setWindowTitle("NYT Connections stats")
         self.setStyleSheet(f"background-color : {self.background_color}")
 
-        self.setCentralWidget(Chat_Loader(self))
+        chat_file_path = Path("./Gui/files/_chat.txt")
+
+        if chat_file_path.is_file():
+            self.chat_loaded()
+        else:
+            chat_loader = Chat_Loader(self)
+            self.setCentralWidget(chat_loader)
+            chat_loader.chat_loaded.connect(self.chat_loaded)
+
+    def chat_loaded(self):
+        self.setCentralWidget(QtWidgets.QLabel("Chat loaded!"))
 
 class Chat_Loader(QtWidgets.QWidget):
+
+    chat_loaded = QtCore.pyqtSignal()
+
     def __init__(self, parent = None):
         super().__init__(parent = parent)
 
@@ -45,8 +59,10 @@ class Chat_Loader(QtWidgets.QWidget):
         
         if popup.exec():
             copy2(popup.selectedFiles()[0], "./Gui/files/_chat.txt")
+            self.chat_loaded.emit()
         else:
             QtWidgets.QMessageBox.critical(self, "Error", "Failed to select file")
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
