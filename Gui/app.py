2a6132b86f0d1cfaad7a945ec934ceb3208d82aa
WilfredSalmon <was29@cam.ac.uk>
2024-08-20 15:13:43 +0100

    reskinned button

--- Gui/app.py
+++ Gui/app.py
@@ -11,14 +11,14 @@ STYLE_SHEET = """
 """
 
 class Main_Window(QtWidgets.QMainWindow):
-    background_color = "#e0ebeb"
+    _background_color = "#e0ebeb"
     
     def __init__(self):
         super().__init__()
         
         self.resize(QtCore.QSize(1000, 600))
         self.setWindowTitle("NYT Connections stats")
-        self.setStyleSheet(f"background-color : {self.background_color}")
+        self.setStyleSheet(f"background-color : {self._background_color}")
 
         chat_file_path = Path("./Gui/files/_chat.txt")
 
@@ -32,6 +32,55 @@ class Main_Window(QtWidgets.QMainWindow):
     def chat_loaded(self):
         self.setCentralWidget(QtWidgets.QLabel("Chat loaded!"))
 
+class Custom_Button(QtWidgets.QPushButton):
+    _border_pen = QtGui.QPen(Qt.GlobalColor.transparent)
+    
+    _unpressed_brush = QtGui.QBrush(QtGui.QColor("#9999ff"))
+    _pressed_brush = QtGui.QBrush(QtGui.QColor("#4d4dff"))
+
+    _unpressed_text_pen = QtGui.QPen(QtGui.QColor("black"))
+    _pressed_text_pen = QtGui.QPen(QtGui.QColor("black"))
+
+    def __init__(self, text : str = None, parent = None):
+        super().__init__(parent = parent)
+        self.button_text = text
+        self.button_held = False
+
+    def sizeHint(self):
+        return QtCore.QSize(100, 30)
+
+    def paintEvent(self, e : QtGui.QPaintEvent):
+        content_rectangle = self.contentsRect()
+        
+        painter = QtGui.QPainter(self)
+        painter.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing)
+
+        painter.setPen(self._border_pen)
+
+        if self.button_held:
+            painter.setBrush(self._pressed_brush)
+        else:
+            painter.setBrush(self._unpressed_brush)
+        
+        painter.drawRoundedRect(content_rectangle, 15 , 15)
+
+        # TODO : can these be combined into a single if statement?
+        if self.button_held:
+            painter.setPen(self._pressed_text_pen)
+        else:
+            painter.setPen(self._unpressed_text_pen)
+
+        painter.drawText(content_rectangle, Qt.AlignmentFlag.AlignCenter, self.button_text)  
+
+    def mousePressEvent(self, e):
+        self.button_held = True
+        self.update()
+
+    def mouseReleaseEvent(self, e):
+        self.button_held = False
+        self.update()
+        self.pressed.emit()
+
 class Chat_Loader(QtWidgets.QWidget):
 
     chat_loaded = QtCore.pyqtSignal()
@@ -40,14 +89,19 @@ class Chat_Loader(QtWidgets.QWidget):
         super().__init__(parent = parent)
 
         label = QtWidgets.QLabel("No chat history loaded", parent = self)
-        button = QtWidgets.QPushButton("Add Chat", parent = self)
+        font = label.font()
+        font.setPointSize(20)
+        label.setFont(font)
+
+        button = Custom_Button("Load chat", parent = parent)
 
         button.pressed.connect(self.load_chat)
 
         layout = QtWidgets.QVBoxLayout()
-        layout.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(label)
-        layout.addWidget(button)
+        layout.addSpacing(100)
+        layout.addWidget(button, alignment = Qt.AlignmentFlag.AlignCenter)
 
         self.setLayout(layout)
         
