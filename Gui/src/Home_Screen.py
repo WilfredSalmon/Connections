78d25c3a6224a5c62236735e48feba34f0f4806a
WilfredSalmon <was29@cam.ac.uk>
2024-08-21 22:50:03 +0100

    refactor

--- Gui/src/Home_Screen.py
+++ Gui/src/Home_Screen.py
@@ -1,75 +1,99 @@
 from PyQt6 import QtCore, QtWidgets
 from PyQt6.QtCore import Qt
-from typing import Dict
+from typing import Dict, List
 from .util.Player import Player
 
+class _Column():
+    def __init__(self, title : str, value_getter, format : str, sortable : bool):
+        self.title = title
+        self.value_getter = value_getter
+        self.format = format
+        self.sortable = sortable
+
+    def get_string(self, player : Player):
+        return f'{self.value_getter(player):{self.format}}'
+
+
 class Home_Screen(QtWidgets.QWidget):
+
+    _columns : List[_Column] = [
+        _Column (
+            "Name",
+            lambda player : player.name,
+            "",
+            False
+        ),
+
+        _Column (
+            "Win Percentage",
+            lambda player : player.total_wins/player.total_puzzles,
+            ".0%",
+            True
+        ),
+
+        _Column (
+            "Longest Streak",
+            lambda player : "0" if player.longest_streak is None else f'{player.longest_streak.length}',
+            "",
+            True
+        ),
+
+        _Column(
+            "Current Streak",
+            lambda player : "0" if player.current_streak is None else f'{player.current_streak.length}',
+            "",
+            True
+        ),
+
+        _Column (
+            "Submitted Puzzles",
+            lambda player : player.total_attempts,
+            "",
+            True
+        ),
+
+        _Column (
+            "Average Categories Found",
+            lambda player : player.total_categories_found/player.total_puzzles,
+            ".2f",
+            True 
+        ),
+
+        _Column (
+            "Average Attempts",
+            lambda player : player.total_attempts/player.total_puzzles,
+            ".2f",
+            True
+        )
+    ]
+
     def __init__(self, name_player_dict : Dict[str, Player], parent : QtWidgets.QWidget = None):
         super().__init__(parent = parent)
         self.name_player_dict = name_player_dict
+
+        self.names = [name for name in name_player_dict]
+        layout = QtWidgets.QGridLayout()
         
-        self.model = _Player_Stats_Model(self.name_player_dict)
-        self.table = QtWidgets.QTableView()
-        self.table.setModel(self.model)
-        self.table.setShowGrid(False)
-        self.table.setWordWrap(True)
-
-
-        layout = QtWidgets.QVBoxLayout()
-        layout.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
-        layout.addWidget(self.table)
+        self.set_headers(layout)
+        self.set_rows(layout)
+        
         self.setLayout(layout)
 
-class _Player_Stats_Model(QtCore.QAbstractTableModel):
-    _headers = ["Name", "Win Percentage", "Longest Streak", "Current Streak", "Total Puzzles", "Average Categories Found", "Average Number of Attempts"]
-    
-    def __init__(self, name_player_dict : Dict[str, Player], parent : QtWidgets.QWidget = None):
-        super().__init__(parent = parent)
-        self.names = [name for name in name_player_dict]
-        self.name_player_dict = name_player_dict
+    def set_headers(self, layout : QtWidgets.QGridLayout):
+        for index, column in enumerate(self._columns):
+            layout.addWidget(
+                QtWidgets.QLabel(column.title),
+                0,
+                index   
+            )
 
-    def data(self, index, role):
-        if role == Qt.ItemDataRole.DisplayRole:
-            name = self.names[index.row()]
+    def set_rows(self, layout : QtWidgets.QGridLayout):
+        for row_index, name in enumerate(self.names):
             player = self.name_player_dict[name]
-
-            match index.column():
-                case 0:
-                    return name
-                
-                case 1:
-                    return f'{player.total_wins/player.total_puzzles:.0%}'
-                
-                case 2:
-                    if player.longest_streak is None:
-                        return 0
-                    
-                    return player.longest_streak.length
-                
-                case 3:
-                    if player.current_streak is None:
-                        return 0
-                    
-                    return player.current_streak.length
-                
-                case 4:
-                    return player.total_puzzles
-                
-                case 5:
-                    return f'{player.total_categories_found/player.total_puzzles:.2f}'
-                
-                case 6:
-                    return f'{player.total_attempts/player.total_puzzles:.2f}'
-                
-    def headerData(self, section, orientation, role):
-        if role == Qt.ItemDataRole.DisplayRole:
-            if orientation == Qt.Orientation.Horizontal:
-                    return self._headers[section]
-            else:
-                return ""
-    
-    def rowCount(self, index):
-        return len(self.names)
-    
-    def columnCount(self, index):
-        return 7
+            
+            for col_index, column in enumerate(self._columns): 
+                layout.addWidget(
+                    QtWidgets.QLabel(column.get_string(player)),
+                    row_index + 1,
+                    col_index
+                )
